a474e9811f084b4847b3b5b74857f4b06dc4c4c7
Iiubomyrpavlyk <lybomir.pavlick@gmail.com>
2023-02-23 13:23:20 +0200

    add deps

--- app/src/main/java/com/project/hub/application/MainActivity.kt
+++ app/src/main/java/com/project/hub/application/MainActivity.kt
@@ -3,7 +3,9 @@ package com.project.hub.application
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.project.hub.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
