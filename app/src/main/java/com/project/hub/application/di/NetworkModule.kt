298eb0243154eb3f15c44ee28680f3d28fd1456b
Iiubomyrpavlyk <lybomir.pavlick@gmail.com>
2023-02-24 15:14:04 +0200

    add network module

---
+++ app/src/main/java/com/project/hub/application/di/NetworkModule.kt
@@ -0,0 +1,58 @@
+package com.project.hub.application.di
+
+import com.project.hub.core.data.network.adapter.ResultAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    const val API_URL: String = "https://project-hub.herokuapp.com/api/"
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return createRetrofit(API_URL, okHttpClient).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideAuthApi(retrofit: Retrofit): Nothing = TODO()
+
+    @Provides
+    @Singleton
+    fun provideProjectApi(retrofit: Retrofit): Nothing = TODO()
+
+    @Provides
+    @Singleton
+    fun provideProfileApi(retrofit: Retrofit): Nothing = TODO()
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(createLoggingInterceptor())
+            .build()
+    }
+
+    private fun createRetrofit(url: String, okHttpClient: OkHttpClient) = Retrofit.Builder()
+        .baseUrl(url)
+        .client(okHttpClient)
+        .addCallAdapterFactory(ResultAdapterFactory())
+        .addConverterFactory(GsonConverterFactory.create())
+
+    private fun createLoggingInterceptor(): Interceptor {
+        return HttpLoggingInterceptor()
+            .setLevel(HttpLoggingInterceptor.Level.BODY)
+    }
+
+}
