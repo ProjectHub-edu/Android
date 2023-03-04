4c2a9dfe46a0c0c89be389a3d4b0e726ab874da9
Volodymyr <labish.vova@gmail.com>
2023-03-04 20:36:32 +0200

    add register method in AuthService

--- app/src/main/java/com/project/hub/feature/auth/data/remote/api/AuthService.kt
+++ app/src/main/java/com/project/hub/feature/auth/data/remote/api/AuthService.kt
@@ -4,6 +4,8 @@ import com.project.hub.core.data.network.result.NetworkResult
 import com.project.hub.core.util.onFailure
 import com.project.hub.feature.auth.data.remote.entymodels.LoginModel
 import com.project.hub.feature.auth.data.remote.entymodels.LoginResponse
+import com.project.hub.feature.auth.data.remote.entymodels.RegisterModel
+import com.project.hub.feature.auth.data.remote.entymodels.RegisterResponse
 import retrofit2.Retrofit
 import javax.inject.Inject
 import javax.inject.Named
@@ -17,6 +19,11 @@ class AuthService @Inject constructor(
     suspend fun login(email: String, password: String): NetworkResult<LoginResponse> {
         val loginModel = LoginModel(email, password)
         return authApi.login(loginModel)
+    }
+
+    suspend fun register(email: String, password: String, username: String): NetworkResult<RegisterResponse> {
+        val registerModel = RegisterModel(email, password, username)
+        return authApi.register(registerModel)
     }
 
 }
